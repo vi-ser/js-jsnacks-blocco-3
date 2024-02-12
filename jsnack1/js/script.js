125de65d3a22cfea556b2c522f18573b3172ecbe
vi_ser <vinceulisse@gmail.com>
2024-02-12 12:38:34 +0100

    Logica snack 1 funzionante

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -2,3 +2,15 @@
 Snack 1:
 Scrivi una funzione che accetti una stringa come argomento e la restituisca girata
 */
+
+
+// console.log(invertString("Ciao come stai?"));
+
+
+function invertString(string) {
+    let newString = "";
+    for (let i = string.length - 1; i > -1; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
