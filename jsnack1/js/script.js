7b2e346f3b6b47b8680a4960dcc39a620cba07e4
vi_ser <vinceulisse@gmail.com>
2024-02-12 13:09:41 +0100

    snack 1 completato

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -2,15 +2,24 @@
 Snack 1:
 Scrivi una funzione che accetti una stringa come argomento e la restituisca girata
 */
+const inputStringEl = document.querySelector("#input-string");
+const outputStringEl = document.querySelector("#output");
+const buttonInvertEl = document.querySelector("#button-invert");
 
 
-// console.log(invertString("Ciao come stai?"));
-
+buttonInvertEl.addEventListener("click",
+    function () {
+        // al click inverto la stringa inserita in input
+        invertString(inputStringEl.value);
+    })
 
 function invertString(string) {
+    // inizializzo una nuova stringa vuota
     let newString = "";
+    // aggiungo alla nuova stringa i caratteri partendo dall'ultimo
     for (let i = string.length - 1; i > -1; i--) {
         newString += string[i];
     }
-    return newString;
+    // mostro nel DOM la nuova stringa
+    outputStringEl.innerHTML = newString;
 }
